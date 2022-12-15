8d75d01b4ac03fc04c1acb7da8fe7340eb65ad59
zsph <edo.spinach@ymail.com>
2022-12-15 17:56:18 +0100

    config.js fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  pageExtensions: ['page.tsx', 'page.jsx', 'api.ts', 'api.js', 'next.ts']
 }
 
 module.exports = nextConfig
