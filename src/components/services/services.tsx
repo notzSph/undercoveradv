24ddbb83ac7c0b0576f81d6d115283aa6b24bf2b
zsph <edo.spinach@ymail.com>
2022-12-18 19:51:57 +0100

    services & popup height fixes

--- src/components/services/services.tsx
+++ src/components/services/services.tsx
@@ -1,22 +1,35 @@
 import { GenericPopup } from '../../share/ui/generic-popup/genericPopup';
+import GenericSection from '../../share/ui/generic-section/genericSection';
 import GenericTile from '../../share/ui/generic-tile/genericTile';
+import { servicesTiles } from '../../share/utils/const.utils';
 import styles from './services.module.scss';
 
+
 export default function ServicesPopup() {
-    return(
+    return (
         <>
-        <GenericPopup id='services' hasSidebar={true} hasScroll={true} 
-        sidebar={
-            <div>ginni ginni</div>
-        }
-        content={
-            <GenericTile id='services' isPortfolio={false} 
-            title={'Custom Web App'}
-            subhead={'Do i need a Website for my business?'}
-            description={'axjacsdsjaskkdsjdac c n idj hij  scskjcdnjskndjk dskjncs sdns jd j dskjj'}
+            <GenericPopup id='services' hasSidebar={true} hasScroll={true}
+                sidebar={
+                    <div>ginni ginni</div>
+                }
+                content={
+                    servicesTiles.map((section, is) => {
+                        return (
+                            <GenericSection key={is} title={section.title}>
+                                {
+                                    section.children.map((tile, it) => {
+                                        return (
+                                            <GenericTile key={it} id='services' isPortfolio={false}
+                                                subhead={tile.subhead}
+                                                description={tile.description}
+                                            />
+                                        )
+                                    })}
+                            </GenericSection>
+                        )
+                    })
+                }
             />
-        }
-        />
         </>
     )
 }
