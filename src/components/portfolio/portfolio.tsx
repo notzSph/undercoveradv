c82bd820a1eb93f5b90aba111e0a2ce7bf062a34
zsph <edo.spinach@ymail.com>
2022-12-18 16:48:08 +0100

    portfolio popup init

---
+++ src/components/portfolio/portfolio.tsx
@@ -0,0 +1,19 @@
+import { GenericPopup } from '../../share/ui/generic-popup/genericPopup'
+import styles from './portfolio.module.scss'
+
+export default function PortfolioPopup() {
+    return (
+        <>
+            <GenericPopup id='portfolio' hasSidebar={true} hasScroll={false}
+                sidebar={
+                    <div>
+                        Minchia zio
+                    </div>
+                }
+                content={
+                    <h1>Scazza minchia ueue</h1>
+                }
+            />
+        </>
+    )
+}
