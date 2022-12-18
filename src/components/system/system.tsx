c8560734d5953faa143245b02e58e90942e10faf
zsph <edo.spinach@ymail.com>
2022-12-18 15:59:00 +0100

    system popup init

--- src/components/system/system.tsx
+++ src/components/system/system.tsx
@@ -1,14 +1,22 @@
 import { GenericPopup } from "../../share/ui/generic-popup/genericPopup";
+import styles from './system.module.scss';
+
 
 export default function SystemPopup() {
     return (
         <>
-        <GenericPopup id="System" hasScroll={true} hasSidebar={true}
-            topBar={
-                <div className="w-100 bg-notWhite">
-                </div>
-            }
-            
+            <GenericPopup id="1" hasScroll={true} hasSidebar={true}
+                sidebar={
+                    <div className='f-column flex-center-all ' style={{height: '600px'}}>
+                        <img src='assets/images/undrcover-logo.png' alt='undrcvr_logo' className={`${styles.systemLogo} pb-5`} />
+                        <h1 className='pb-5'>UndercoverADV</h1>
+                        <h4 className='pb-4'>Your Parnter in Crime</h4>
+                        <h6>Since 2009</h6>
+                    </div>
+                }
+                content={
+                    <h1> wewe </h1>
+                }
             />
         </>
     )
