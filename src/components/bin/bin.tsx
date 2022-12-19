8c0ad0cb6f5ff92b03d9c9b24d8713f9a495e8b6
zsph <edo.spinach@ymail.com>
2022-12-19 16:38:33 +0100

    i18n implementation in bin component

--- src/components/bin/bin.tsx
+++ src/components/bin/bin.tsx
@@ -1,18 +1,31 @@
+import { GetStaticPropsContext } from 'next'
+import { useTranslations } from 'next-intl'
 import { GenericPopup } from '../../share/ui/generic-popup/genericPopup'
 import { undercoverBinLogo } from '../../share/utils/const.utils'
 import styles from './bin.module.scss'
 
 export default function BinPopup() {
-return(
-<>
-<GenericPopup id='bin' hasSidebar={false} hasScroll={false} 
-content={
-<div className='w-100 h-100 f-column flex-center-all '>
-    <img src={undercoverBinLogo} className='pb-5' />
-    <h2 className='pb-4'>Trash Bin is Empty</h2>
-    <p>Be honest, would you choose a partner that falls for the old bin trick?</p>
-</div>
-} />
-</>
-)
+
+    const i18n = useTranslations('Bin')
+
+    return (
+        <>
+            <GenericPopup id='bin' hasSidebar={false} hasScroll={false}
+                content={
+                    <div className='w-100 h-100 f-column flex-center-all '>
+                        <img src={undercoverBinLogo} className='pb-5' style={{width: '250px'}} />
+                        <h2 className='pb-4'>{i18n('claim')}</h2>
+                        <p>{i18n('quote')}</p>
+                    </div>
+                } />
+        </>
+    )
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+    return {
+        props: {
+            messages: (await import(`../../share/lang/${locale}.json`)).default,
+        }
+    }
 }
