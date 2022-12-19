2a17351fb895d33ae9a378bc365aefc574e4a2ce
zsph <edo.spinach@ymail.com>
2022-12-19 01:05:31 +0100

    i18n init

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  pageExtensions: ['page.tsx', 'page.jsx', 'api.ts', 'api.js', 'next.ts']
+  pageExtensions: ['page.tsx', 'page.jsx', 'api.ts', 'api.js', 'next.ts'],
+  i18n: {
+    locales: ['en', 'it'],
+    defaultLocale: 'en'
+  }
 }
 
 module.exports = nextConfig
