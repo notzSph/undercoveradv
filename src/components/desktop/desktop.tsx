0db6cc91ce640c3aedfa0ecd453b3c49da1c153b
zsph <edo.spinach@ymail.com>
2022-12-22 23:29:41 +0100

    folderTile refactor

--- src/components/desktop/desktop.tsx
+++ src/components/desktop/desktop.tsx
@@ -4,56 +4,45 @@ import PortfolioPopup from "../portfolio/portfolio";
 import ServicesPopup from "../services/services";
 import SystemPopup from "../system/system";
 import FolderTile from "../../share/ui/folder-tile/folderTile";
-import { hiBandFolders, loBandFolders, midBandFolders } from "../../share/utils/const.utils";
+import { folderTiles, loBandFolders, midBandFolders } from "../../share/utils/const.utils";
 import styles from './desktop.module.scss'
 import { useState } from "react";
+import { isPropertySignature } from "typescript";
 
 export default function Desktop() {
 
     const band = `${styles.band} flex-center-all `;
     const isLargeLayout: boolean = true;
 
-    const [ activePopup, setActivePopup ] = useState<number | undefined>() 
+    const [activePopup, setActivePopup] = useState<number | undefined>()
 
 
     return (
         <>
             {/* Main Container */}
             <div className="w-100 vh-100 f-column a-center j-between p-3">
-
-                {/* Hi Band Container */}
-                <div className={band} style={{ width: isLargeLayout ? '80%' : '100%' }}>
-                    {hiBandFolders.map((folder, i) => {
+                <>
+                    {folderTiles.map((folder, i) => {
                         return (
-                            <FolderTile key={i} id={folder.id} icon={folder.icon} name={folder.name} onClick={() => setActivePopup(folder.id)}/>
+                            <FolderTile key={i} id={folder.id}
+                                icon={folder.icon}
+                                name={folder.name}
+                                onClick={() => setActivePopup(folder.id)}
+                                style={{
+                                    top: folder.top,
+                                    left: folder.left
+                                }}
+                            />
                         )
                     })}
-                </div>
-
-                {/* Mid Band Container */}
-                <div className={band} style={{ width: isLargeLayout ? '60%' : '100%' }}>
-                    {midBandFolders.map((folder, i) => {
-                        return (
-                            <FolderTile key={i} id={folder.id} icon={folder.icon} name={folder.name} onClick={() => setActivePopup(folder.id)}/>
-                        )
-                    })}
-                </div>
-
-                {/* Lo Band Container */}
-                <div className={band} style={{ width: isLargeLayout ? '90%' : '100%' }}>
-                    {loBandFolders.map((folder, i) => {
-                        return (
-                            <FolderTile key={i} id={folder.id} icon={folder.icon} name={folder.name} onClick={() => setActivePopup(folder.id)} />
-                        )
-                    })}
-                </div>
+                </>
             </div>
 
             {/* Popups */}
-            <SystemPopup isActive={activePopup === 1} onClose={() => setActivePopup(undefined)}  />
-            <AboutPopup isActive={activePopup === 2} onClose={() => setActivePopup(undefined)} />
-            <ServicesPopup isActive={activePopup === 3} onClose={() => setActivePopup(undefined)}/>
-            <PortfolioPopup isActive={activePopup === 4} onClose={() => setActivePopup(undefined)}/>
+            <SystemPopup isActive={activePopup === 1} onClose={() => setActivePopup(undefined)} />
+            <ServicesPopup isActive={activePopup === 2} onClose={() => setActivePopup(undefined)} />
+            <AboutPopup isActive={activePopup === 3} onClose={() => setActivePopup(undefined)} />
+            <PortfolioPopup isActive={activePopup === 4} onClose={() => setActivePopup(undefined)} />
             <BinPopup isActive={activePopup === 6} onClose={() => setActivePopup(undefined)} />
         </>
     )
--- src/share/ui/folder-tile/folderTile.tsx
+++ src/share/ui/folder-tile/folderTile.tsx
@@ -6,9 +6,15 @@ export default function FolderTile(props: FolderTileProps) {
     return (
         <>
             {/* Container */}
-            <div className="w-100 f-column a-center" onClick={props.onClick}>
-                <img src={props.icon} style={{ width: '60px' }} alt={props.name} className='pb-4'/>
-                <h6>{props.name}</h6>
+            <div className="absolute" onClick={props.onClick} style={{
+                top: (props.top ? (props.top) : undefined),
+                left: (props.left ? (props.left) : undefined),
+                ...props.style
+            }}>
+                <div className="f-column a-center">
+                <img src={props.icon} style={{ width: '80px' }} alt={props.name} className='pb-5'/>
+                <span>{props.name}</span>
+                </div>
             </div>
         </>
     )
